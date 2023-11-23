a089ec773ac4393bad33328ba0df32422584b19c
Mozammal Hossain <Mozammalruet@gmail.com>
2023-11-24 03:48:33 +0600

    initial commit

---
+++ lib/src/core/utils/assets.dart
@@ -0,0 +1,16 @@
+class Assets {
+  static const String imagePath = 'assets/images';
+
+  static const String appLogo = '$imagePath/logo.svg';
+  static const String googleLogo = '$imagePath/google_logo.svg';
+  static const String facebookLogo = '$imagePath/facebook_logo.svg';
+  static const String appleLogo = '$imagePath/apple_logo.svg';
+  static const String backIcon = '$imagePath/back_icon.svg';
+  static const String calenderIcon = '$imagePath/calender_icon.png';
+  static const String male = '$imagePath/male_icon.png';
+  static const String female = '$imagePath/female_icon.png';
+  static const String remove = '$imagePath/remove_icon.svg';
+  static const String expandIcon = '$imagePath/expand_icon.png';
+  static const String mediaIcon = '$imagePath/media_icon.svg';
+  static const String cameraIcon = '$imagePath/camera_icon.svg';
+}
